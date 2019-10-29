94a790a68e6d2676fd95dc125cbb5d5a5af9b7d9
Jem <jemalvarez22@gmail.com>
2019-10-29 16:16:39 -0400

    Initial commit

---
+++ Assets/_Scripts/Ship/ShipShooter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooter : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+
+    [SerializeField] float fireRate = 1f;
+
+    Coroutine shootingCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shootingCoroutine = StartCoroutine(ContinuousShooting());
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopCoroutine(shootingCoroutine);
+        }
+    }
+
+    void Shoot()
+    {
+        var bullet = Instantiate(bulletPrefab, transform.position, transform.parent.rotation) as GameObject;
+        bullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * 200f);
+    }
+
+    IEnumerator ContinuousShooting()
+    {
+        while(true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+}
